4b6a068c59628f0eef59d6ef36b588693b0da62f
Brighton Zhang <75390931+BrightonZhang1@users.noreply.github.com>
2022-02-25 00:24:48 +0000

    Same drawing as the last assignment, but uses width and height variables.

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,44 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
+
+	/**
+   * A program Sketch.java that visually sketches a picture of a house on a grassy plain using width and height variables.
+	 * @author: B. Zhang
+	 * date: 2022-02-24
    */
+
+	
   public void settings() {
-	// put your size call here
+		//size of the window
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+  	background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+		
+	// Using the ratio of each shape dimension, I divided/multiplied it with the user set width/height 
+	// Sky
+    fill(52, 223, 235);
+    rect(width * 0, height * 0, width, height * 0.75f);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// House Body
+		fill(240, 65, 65);
+		rect(width/2, height/(400f/175f), width/(400f/150f), height/(400f/125f));
+	
+	// Chimney
+		fill(91, 98, 110);
+		rect(width/(400f/325f), height/(400f/110f), width/(400f/25f), height/(400f/60f));
+		
+	// Roof Body
+		fill(91, 98, 110);
+		triangle(width/(400f/175f), height/(400f/175f), width/(400f/375f), height/(400f/175f), width/(400f/275f), height/(400f/100f));
+
+	// Sun
+		fill(235, 222, 52);
+		ellipse(width/(400f/25f), height/(400f/25f), width/(400f/100f), height/(400f/100f));
   }
-  
-  // define other methods down here.
 }
